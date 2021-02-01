a4ccb26fe08773cefb0e0ead61cc1f91ee2a8e9d
andrew <ap@ampiusa.com>
2021-01-31 19:28:11 -0800

    JS update

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,3 +1,13 @@
+var likeBtn = document.getElementById('like-btn');
+var dislikeBtn = document.getElementById('dislike-btn')
+var favoriteBtn = document.getElementById('favorite-btn')
+var favClass = document.getElementById('favorite')
+var likeCount = document.getElementById('like-cnt')
+var disLikeCount = document.getElementById('dislike-cnt')
+var headerQuote = document.getElementById('header-quote')
+var favoriteList = document.getElementById('favorite-list')
+
+
 // Variable to hold images of Kanye West
 var kanyeImg = [
   "https://theartsdesk.com/sites/default/files/styles/mast_image_landscape/public/mastimages/Kanye-West-ft-T.I.-Ye-Vs.-The-People-768x768.png?itok=ijW2BquS", 
@@ -36,9 +46,12 @@ function displayDate() {
     })
     .then(function(response){
         var kanyeQuote = response.quote
+        quotesObj.quote.push(kanyeQuote)
+        favoriteQuotesObj.quote.push(kanyeQuote)
         document.getElementById("kanyeQuote").innerHTML = `"${kanyeQuote}"`
         kanyeRandom();
         document.getElementById("kanyeName").innerHTML = "Kanye West"
+        socialStatus()
     })
     }
   
@@ -52,8 +65,11 @@ function displayDate() {
     })
     .then(function(response){
         var taylorQuote = response.quote
+        quotesObj.quote.push(taylorQuote)
+        favoriteQuotesObj.quote.push(taylor)
         document.getElementById("taylorQuote").innerHTML = `"${taylorQuote}"`
         document.getElementById("taylorName").innerHTML = "Taylor Swift"
+        socialStatus()
     })
 
     // API call to get random Taylor image
@@ -65,6 +81,253 @@ function displayDate() {
           document.getElementById("taylorImg").setAttribute("src", response.url)
       })
     }
+
+    // -----temp storage of current quote obj------//
+    var quotesObj = {
+      quote: [],
+      likes: [],
+      dislikes: []
+    };
+
+    var testFun = function(){
+      headerQuote.innerHTML = '"' + quotesObj.quote[0] + '"'
+    }
+
+// -----------------------------------------------//
+// --------Save Like count into local storage-----//
+
+var saveLikesToStorage = function() {
+  var getLocalStorage = JSON.parse(localStorage.getItem(quotesObj.quote))
+  
+  if (getLocalStorage === null) {
+    quotesObj.likes++
+    localStorage.setItem(quotesObj.quote,JSON.stringify(quotesObj));
+    updateSocialStatus()    
+  } 
+  else {
+      var currentLikes = JSON.parse(localStorage.getItem(quotesObj.quote))
+      quotesObj.likes++;
+      localStorage.setItem(quotesObj.quote,JSON.stringify(quotesObj));
+      updateSocialStatus()        
+  }  
+}
+// -----------------------------------------------//
+
+// ----------Save Dislike count to local storage-----//
+
+var saveDislikesToStorage = function() {
+  var getLocalStorage = JSON.parse(localStorage.getItem(quotesObj.quote))
+  
+  if (getLocalStorage === null) {
+    quotesObj.dislikes++
+    localStorage.setItem(quotesObj.quote,JSON.stringify(quotesObj));
+    updateSocialStatus()
+  } 
+  else {
+
+    var currentSocial = JSON.parse(localStorage.getItem(quotesObj.quote))    
+     
+      quotesObj.dislikes++;        
+      localStorage.setItem(quotesObj.quote,JSON.stringify(quotesObj));
+      updateSocialStatus()
+  }  
+}
+// -----------------------------------------------//
+
+// --------------Load Social Status--------------//
+
+var socialStatus = function () {
+ 
+  var getLocalStorage = JSON.parse(localStorage.getItem(quotesObj.quote))    
+  
+  if(getLocalStorage !== null){
+
+      if(getLocalStorage.likes.length !== 0 && getLocalStorage.dislikes.length !== 0) {
+        quotesObj.likes.push(getLocalStorage.likes)
+        quotesObj.dislikes.push(getLocalStorage.dislikes)
+        likeCount.innerHTML = quotesObj.likes
+        disLikeCount.innerHTML = quotesObj.dislikes
+      } 
+      else if (getLocalStorage.likes.length !== 0 && getLocalStorage.dislikes.length === 0){
+        quotesObj.likes.push(getLocalStorage.likes)  
+        likeCount.innerHTML = quotesObj.likes
+        disLikeCount.innerHTML = quotesObj.dislikes  
+      } 
+      else if (getLocalStorage.likes.length === 0 && getLocalStorage.dislikes.length !== 0){
+       quotesObj.dislikes.push(getLocalStorage.dislikes)
+       likeCount.innerHTML = quotesObj.likes
+       disLikeCount.innerHTML = quotesObj.dislikes
+      }
+  }   
+}
+// -----------------------------------------------//
+
+// ---------------Update Social status------------//
+
+var updateSocialStatus = function() {
+
+  var getLocalStorage = JSON.parse(localStorage.getItem(quotesObj.quote[0]))
+    likeCount.innerHTML = getLocalStorage.likes
+    disLikeCount.innerHTML = getLocalStorage.dislikes  
+}
+//-------------------------------------------------//
+
+// -------Favorite Button save to local storage----//
+
+var favBtnHandler = function(event){
+  var favBtn = favoriteBtn.value
+  
+  if(favBtn === "not-favorite") {  
+      favoriteBtn.value = "is-favorite"
+      favClass.classList.add('fav-btn')
+      favoriteQuote()
+      
+      }
+      else if (favBtn === 'is-favorite'){
+      favoriteBtn.value="not-favorite"
+      favClass.classList.remove('fav-btn')
+      deleteFavorite() 
+         
+      }
+  }
+  // -----------------------------------------------//
+
+  // ----------Favorite Quotes temp object---------//
+var favoriteQuotesObj = {
+  quote: []
+}
+// -----------------------------------------------//
+
+// ----------Set selected quote to local storage---//
+
+var favoriteQuote = function() {
+  var getLocalStorage = JSON.parse(localStorage.getItem('favoriteQuotes'))
+
+  if (getLocalStorage === null) {    
+    localStorage.setItem('favoriteQuotes', JSON.stringify(favoriteQuotesObj))     
+  } 
+  else {
+    var favorite = JSON.parse(localStorage.getItem('favoriteQuotes'))    
+
+    for(var i = 0; i < favorite.quote.length; i++){
+      var storedFavorite = favorite.quote[i]
+      favoriteQuotesObj.quote.push(storedFavorite)
+      localStorage.removeItem('favoriteQuotes')         
+
+      if( favoriteQuotesObj.quote.length > 5) {
+        favoriteQuotesObj.quote.splice(4,1)
+        localStorage.setItem('favoriteQuotes', JSON.stringify(favoriteQuotesObj))        
+      } else {
+
+        localStorage.setItem('favoriteQuotes', JSON.stringify(favoriteQuotesObj))
+      }      
+    }         
+  }  
+}
+// -----------------------------------------------//
+
+// ---------Delete "un" favorite quote-----------//
+ 
+var deleteFavorite = function(){
+  favoriteQuotesObj ={
+    quote: []
+  }
+  var favorite = JSON.parse(localStorage.getItem('favoriteQuotes'))
+
+    for(var i = 0; i < favorite.quote.length; i++){
+
+      var storedFavorite = favorite.quote[i]
+      favoriteQuotesObj.quote.push(storedFavorite)            
+    }
+
+    localStorage.removeItem('favoriteQuotes')
+ 
+    var favDeleteQuote = quotesObj.quote[0]
+    console.log('favorite delete',favDeleteQuote);
+    favoriteQuotesObj.quote.splice(favoriteQuotesObj.quote.indexOf(favDeleteQuote),1) 
+    
+    if(favoriteQuotesObj.quote.length === 0) {
+      return
+    } else{
+      localStorage.setItem('favoriteQuotes', JSON.stringify(favoriteQuotesObj))
+    }         
+    localStorage.setItem('favoriteQuotes', JSON.stringify(favoriteQuotesObj))
+    }
+  // ------------------------------------------------------------------------//
+
+  // var deleteFavoriteModal = function(value){
+  //   var favQuote = value.innerHTML
+  //   console.log(favQuote.innerHTML);
+
+  // }
+   
+  // -----------------------------------------------------------------------//
+
+   // ---------build favorite list------------------------------------//
+
+   var buildFavorite = function(){
+    var favorite = JSON.parse(localStorage.getItem('favoriteQuotes'))
+    if(favorite !== null) {    
+       
+      for(var i =0; i< favorite.quote.length; i++){
+        var favListItem = document.createElement('li');
+        favListItem.setAttribute('class', 'collection-item');
+
+        var favRowDiv = document.createElement('div');
+        favRowDiv.setAttribute('class', "row");
+
+        var favColDiv = document.createElement('div');
+        favColDiv.setAttribute('class', 'col s12 m12');
+
+        var favColorDiv = document.createElement('div')
+        favColorDiv.setAttribute('class', 'card blue-grey lighten-5')
+
+        var favCardDiv =document.createElement('div');
+        favCardDiv.setAttribute('class', 'card-content black-text');
+
+        var favCardSpan = document.createElement('span');
+        favCardSpan.setAttribute('class', 'card-title');
+
+        // var favCardSpanAttr = document.createElement('a');
+        // favCardSpanAttr.setAttribute('class', 'waves-effect waves btn-flat right');          
+        // favCardSpanAttr.setAttribute('onclick', 'deleteFavoriteModal(this)')
+
+        // var favCardSpanAttrIcon = document.createElement('i')
+        // favCardSpanAttrIcon.setAttribute('class', 'material-icons')
+        // favCardSpanAttrIcon.innerHTML = "close"
+
+        var favSpanParagraph = document.createElement('p');
+        favSpanParagraph.innerHTML = favorite.quote[i]
+
+        var favCardAction = document.createElement('div')
+        favCardAction.setAttribute('class', 'card-action')
+       
+        
+        favoriteList.appendChild(favListItem)
+        favListItem.appendChild(favRowDiv)
+        favRowDiv.appendChild(favColDiv)
+        favColDiv.appendChild(favColorDiv)
+        favColorDiv.appendChild(favCardDiv)
+        favCardDiv.appendChild(favCardSpan)
+        // favCardSpan.append(favCardSpanAttr)
+        // favCardSpanAttr.append(favCardSpanAttrIcon)
+        favCardSpan.appendChild(favSpanParagraph)
+        favCardDiv.appendChild(favCardAction)
+      }
+
+    } 
+  }
+  // ------------------------------------------------------------------//
+
+      // ---favorites Modal------//
+      document.addEventListener('DOMContentLoaded', function() {
+        var elems = document.querySelectorAll('.modal');
+        var instances = M.Modal.init(elems, 'opacity');
+      });
+  
+
+
+
   
   kanyeEl();
   taylorEl();
